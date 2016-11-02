d51bf0b68ca3d820b5106ca8cdd90623b0f24307
ftteotw <i@iwvwi.com>
2016-11-02 20:59:15 +0800

    #!/bin/bash git.sh

---
+++ 12.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+unset var
+while [ "$var" != "end" ]
+do
+    echo -n "please input a number: "
+    read var
+    if [ "$var" = "end" ]
+    then
+        break
+    fi
+    echo "var is $var"
+done
